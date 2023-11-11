ad051d5de8b72d63acc5e7f90f5dbff32e57bfb6
carloshoras <carloshoras>
2023-11-11 20:12:52 +0100

    Acabado de depurar

--- js/script.js
+++ js/script.js
@@ -1,44 +1,40 @@
 const piedra = document.querySelector('.boton-jugada[data-jugada="piedra"]')
 const papel = document.querySelector('.boton-jugada[data-jugada="papel"]')
 const tijera = document.querySelector('.boton-jugada[data-jugada="tijera"]')
-let jugada = "";
-let jugadaOrdenador = "";
-let resultado = "";
+const contadorusuario = document.getElementById("contador-usuario");
+const contadorordenador = document.getElementById("contador-ordenador");
+let puntosusuarios = 0
+let puntosordenador = 0
+const resultados = document.getElementById("resultados")
 
 let posibilidades = ["piedra", "papel", "tijera"]
 
 let comparar = function (indiceUsuario) {
-
-    let indiceOrdenador = Math.floor(Math.random() * 3)
-    console.log(indiceOrdenador)
+    let resultado = "";
+    let indiceOrdenador = Math.floor(Math.random() * 3);
     if (indiceUsuario == indiceOrdenador) {
         resultado = "Empate"
     } else if (indiceUsuario == indiceOrdenador -1 ) {
+        puntosordenador = puntosordenador + 1;
         resultado = "Gana ordenador"
     } else if (indiceUsuario == 2 && indiceOrdenador==0) {
+        puntosordenador = puntosordenador + 1;
         resultado = "Gana ordenador";
     } else {
+        puntosusuarios = puntosusuarios +1;
         resultado = "Ganas tú";
     }
-
-    const resultados = document.getElementById("resultados")
     resultados.innerHTML=`<p>Tú ha elegido ${posibilidades[indiceUsuario]} y el ordenador ha elegido ${posibilidades[indiceOrdenador]} . ${resultado}</p>`
+    contadorordenador.textContent =`Puntos de la máquina: ${puntosordenador}`
+    contadorusuario.textContent =`tus puntos: ${puntosusuarios}`
 }
+
 piedra.addEventListener("click", function() {
-    jugada = 0
-    console.log(jugada)
-    console.log(comparar(jugada));
-   
+    comparar(0);
 })
-
 papel.addEventListener("click", function() {
-    jugada = 1
-    console.log(jugada)
-    console.log(comparar(jugada));
+    comparar(1);
 })
-
 tijera.addEventListener("click", function() {
-    jugada = 2
-    console.log(jugada)
-    console.log(comparar(jugada));
+    comparar(2);
 })
